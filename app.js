69512b7b6199e213c63af61cddc56c824e8d7ed2
Cynthia Tong <cynthiatong16@gmail.com>
2016-12-12 11:12:23 +0400

    Fixed port issue

--- app.js
+++ app.js
@@ -110,6 +110,6 @@ app.get("*", function(req,res){
 });
 
 
-app.listen(3000);
-console.log('Express started on port 3000');
-
+var port = process.env.PORT || 3000; 
+app.listen(port);
+console.log('Express started on port ' + port);
